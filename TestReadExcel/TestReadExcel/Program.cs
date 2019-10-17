884617a53582d9165e5fe07aa5aa742c01391cfb
Jonathan <jnaranjv@gmail.com>
2019-10-17 09:10:50 +0200

    Start

---
+++ TestReadExcel/TestReadExcel/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestReadExcel.Model;
+
+namespace TestReadExcel
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var apps = LoadDataApplications(@".\Config\data.xlsx");
+			foreach (var item in apps)
+			{
+				Console.WriteLine("Code: {0}", item.Code);
+				Console.WriteLine("Name: {0}\n", item.Name);
+			}
+
+			Console.ReadKey();
+		}
+		private static List<MApplication> LoadDataApplications(string configFile)
+		{
+			DataTable excelDT = SpreadSheet.GetSheetData(configFile, "APPLICATIONS");
+			List<MApplication> apps = new List<MApplication>();
+
+			foreach (DataRow row in excelDT.Rows)
+			{
+				try
+				{
+					if (!string.IsNullOrEmpty(row["CODE"].ToString()))
+					{
+						var app = new MApplication
+						{
+							Code = Convert.ToInt32(row["CODE"]),
+							Name = row["NAME"].ToString()
+						};
+						apps.Add(app);
+					}
+				}
+				catch (Exception ex)
+				{
+					throw ex;
+				}
+			}
+			return apps;
+		}
+	}
+}
---
+++ TestReadExcel/TestReadExcel/SpreadSheet.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace TestReadExcel
+{
+	public static class SpreadSheet
+	{
+		/// <summary>
+		/// Reader of configfile
+		/// </summary>
+		/// <param name="path"></param>
+		/// <param name="sheetName"></param>
+		/// <returns></returns>
+		public static DataTable GetSheetData(string path, string sheetName)
+		{
+			string localFile = path;
+			SpreadsheetDocument excelFile = SpreadsheetDocument.Open(localFile, false);
+			//Get Sheet Reference
+			Sheet sheet = excelFile.WorkbookPart.Workbook.Descendants<Sheet>().FirstOrDefault(s => s.Name == sheetName);
+			//Get Sheet Rows
+			string relationshipId = sheet.Id.Value;
+			WorksheetPart worksheetPart = (WorksheetPart)excelFile.WorkbookPart.GetPartById(relationshipId);
+			Worksheet workSheet = worksheetPart.Worksheet;
+			SheetData sheetData = workSheet.GetFirstChild<SheetData>();
+			IEnumerable<Row> rows = sheetData.Descendants<Row>();
+			//Create Datatable
+			DataTable dt = new DataTable(sheet.Name);
+			//Datatable Header
+			foreach (Cell cell in rows.ElementAt(0))
+			{
+				dt.Columns.Add(GetCellValue(excelFile, cell));
+			}
+			//Datatable Data
+			foreach (Row row in rows)
+			{
+				AddNewRow(excelFile, dt, row);
+			}
+			if (dt.Rows.Count > 0) dt.Rows.RemoveAt(0);
+			excelFile.Close();
+			return dt;
+		}
+		/// <summary>
+		/// This method reads the content of each excel cell
+		/// </summary>
+		/// <param name="document">SpreadsheetDocument</param>
+		/// <param name="cell">Cell</param>
+		/// <returns>string</returns>
+		private static string GetCellValue(SpreadsheetDocument document, Cell cell)
+		{
+			SharedStringTablePart stringTablePart = document.WorkbookPart.SharedStringTablePart;
+			string value;
+			if (cell.CellValue == null)
+			{
+				value = "";
+			}
+			else
+			{
+				value = cell.CellValue.InnerXml;
+			}
+			if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+			{
+				return stringTablePart.SharedStringTable.ChildElements[int.Parse(value)].InnerText;
+			}
+			else
+			{
+				return value;
+			}
+		}
+		/// <summary>
+		/// This method creates a new row in DataTable
+		/// </summary>
+		/// <param name="excelFile"></param>
+		/// <param name="dt"></param>
+		/// <param name="row"></param>
+		private static void AddNewRow(SpreadsheetDocument excelFile, DataTable dt, Row row)
+		{
+			DataRow tempRow = dt.NewRow();
+			for (int i = 0; i < row.Descendants<Cell>().Count(); i++)
+			{
+				tempRow[i] = GetCellValue(excelFile, row.Descendants<Cell>().ElementAt(i));
+			}
+			dt.Rows.Add(tempRow);
+		}
+	}
+}
